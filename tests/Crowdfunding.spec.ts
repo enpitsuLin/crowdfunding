2f3967bb376d0a5eb2a6177499ae0cd020ca325b
enpitsulin <enpitsulin@gmail.com>
2024-06-22 17:22:30 +0800

    feat: crowdfunding contracts

---
+++ tests/Crowdfunding.spec.ts
@@ -0,0 +1,50 @@
+import { Blockchain, SandboxContract } from '@ton/sandbox';
+import { toNano } from '@ton/core';
+import { Crowdfunding } from '../wrappers/Crowdfunding';
+import { getUnixTimestampNow } from './utils';
+import '@ton/test-utils';
+
+describe('Crowdfunding', () => {
+    let blockchain: Blockchain;
+    let crowdfunding: SandboxContract<Crowdfunding>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        const deployer = await blockchain.treasury('deployer');
+
+        crowdfunding = blockchain.openContract(await Crowdfunding.fromInit(deployer.getSender().address, 0n));
+
+        const deployResult = await crowdfunding.send(
+            deployer.getSender(),
+            {
+                value: toNano('0.05'),
+            },
+            {
+                $$type: 'StartCrowdfunding',
+                creator: deployer.getSender().address,
+                params: {
+                    $$type: 'CrowdfundingParams',
+                    title: '',
+                    description: '',
+                    targetContribution: 0n,
+                    minContribution: 0n,
+                    deadline: BigInt(getUnixTimestampNow()),
+                    beneficiary: deployer.getSender().address,
+                },
+            },
+        );
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: crowdfunding.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and crowdfunding are ready to use
+    });
+});
---
+++ wrappers/CrowdfundingFactory.compile.ts
@@ -0,0 +1,9 @@
+import { CompilerConfig } from '@ton/blueprint';
+
+export const compile: CompilerConfig = {
+    lang: 'tact',
+    target: 'contracts/crowdfunding_factory.tact',
+    options: {
+      debug: true,
+  },
+};
