0b2949732e34b2c8a60dc865ab3d05eca97fe4fd
enpitsulin <enpitsulin@gmail.com>
2024-06-22 18:45:53 +0800

    chore: script

--- contract/scripts/deployCrowdfunding.ts
+++ contract/scripts/deployCrowdfunding.ts
@@ -1,22 +1,5 @@
-import { toNano } from '@ton/core';
-import { Crowdfunding } from '../wrappers/Crowdfunding';
 import { NetworkProvider } from '@ton/blueprint';
 
 export async function run(provider: NetworkProvider) {
-    const crowdfunding = provider.open(await Crowdfunding.fromInit());
-
-    await crowdfunding.send(
-        provider.sender(),
-        {
-            value: toNano('0.05'),
-        },
-        {
-            $$type: 'Deploy',
-            queryId: 0n,
-        }
-    );
-
-    await provider.waitForDeploy(crowdfunding.address);
-
-    // run methods on `crowdfunding`
+    throw new Error('to be implemented') 
 }
