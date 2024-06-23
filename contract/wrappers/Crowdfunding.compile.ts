4d99278d7c551b53023fa061e3900fe539e33030
enpitsulin <enpitsulin@gmail.com>
2024-06-23 16:39:05 +0800

    build: remove child contract build config

--- contract/wrappers/Crowdfunding.compile.ts
+++
@@ -1,9 +0,0 @@
-import type { CompilerConfig } from '@ton/blueprint'
-
-export const compile: CompilerConfig = {
-  lang: 'tact',
-  target: 'contracts/crowdfunding.tact',
-  options: {
-    debug: true,
-  },
-}
