94e3c90daa9b75e5000cd0579b96b2d286bfc2d7
enpitsulin <enpitsulin@gmail.com>
2024-06-24 10:08:03 +0800

    chore: remove unused export

--- contract/tests/utils.ts
+++ contract/tests/utils.ts
@@ -1,25 +1,3 @@
-import { Contract } from "@ton/core";
-import { Blockchain } from "@ton/sandbox";
-
 export function getUnixTimestampNow(): number {
   return Math.floor(Date.now() / 1000)
-}
-
-export async function initContract<C extends Contract, A = C[]>(getContract: () => C | Promise<C>) {
-  const blockchain = await Blockchain.create()
-  const deployer = await blockchain.treasury('deployer')
-  const c = getContract()
-  const contract = blockchain.openContract(
-    c instanceof Promise ? await c : c
-  )
-
-  return {
-    blockchain,
-    deployer,
-    contract
-  }
-}
-
-export async function initBlockchain() {
-
 }
